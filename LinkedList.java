af07cf53342f5d77dfff80ccf4ab978ad2fd4f40
Srikanthnaid <ksru044gmail.com>
2022-09-05 17:14:19 +0530

    Ability to create Linked

---
+++ LinkedList.java
@@ -0,0 +1,19 @@
+
+public class LinkedList {
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) {
+		LinkedListOperations operations = new LinkedListOperations();
+		INode head;
+
+		Node<Integer> myFirstNode = new Node<>(70);
+		Node<Integer> mySecondNode = new Node<>(30);
+		Node<Integer> myThirdNode = new Node<>(56);
+
+		head = operations.addNode(myFirstNode);
+		operations.displayNode(head);
+		head = operations.addNode(mySecondNode);
+		operations.displayNode(head);
+		head = operations.addNode(myThirdNode);
+		operations.displayNode(head);
+	}
+}
